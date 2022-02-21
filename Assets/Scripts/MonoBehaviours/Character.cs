53624a64b17944899316649f555879e87ef84139
bloges56 <brady.drew.logan@gmail.com>
2022-02-21 17:55:11 -0500

    ch5

---
+++ Assets/Scripts/MonoBehaviours/Character.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+
+    public int hitPoints;
+    public int maxHitPoints;
+ 
+}
